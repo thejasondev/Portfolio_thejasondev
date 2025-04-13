75bebf182c7a7a14db1aaeac54d99b055bce03a3
thejasondev <jason.guerra1002@gmail.com>
2025-04-13 15:05:13 -0500

    implement SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,15 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://thejasondev.vercel.app",
-  integrations: [tailwind(), icon(), sitemap()],
+  integrations: [
+    tailwind(),
+    icon(),
+    sitemap({
+      filter: (page) => page !== "https://thejasondev.vercel.app/not-found",
+      customPages: ["https://thejasondev.vercel.app/"],
+      changefreq: "monthly",
+      priority: 1.0,
+      lastmod: new Date(),
+    }),
+  ],
 });
